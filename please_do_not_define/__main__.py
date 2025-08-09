6e3b6d020a1f8f0d78c6bdde1a29e83b0105bcac
Locked-chess-official <13140752715@163.com>
2025-08-09 09:23:21 +0800

    Update __main__.py

--- please_do_not_define/__main__.py
+++ please_do_not_define/__main__.py
@@ -4,6 +4,7 @@ from please_do_not_define.__version__ import __version__
 import sys
 import os
 
+
 def analyse_code(code):
     all_name = get_name_usages_with_location(code)
     illegal_name_dict = {}
@@ -12,53 +13,65 @@ def analyse_code(code):
             illegal_name_dict[key] = value
     return illegal_name_dict
 
-if len(sys.argv) == 1 or (len(sys.argv) == 2 and sys.argv[1] == '-h'):
-    sys.stderr.write(
-        '''Usage:
-    checkname filename: analyse the name in the python code file
-    checkname -s code: analyse the name in the code
-    checkname -h: for help
-    checkname -v: show the version''')
 
-elif len(sys.argv) == 2 and sys.argv[1] == '-v':
-    print("version:", __version__)
+def main():
+    args = sys.argv[1:]
 
-elif len(sys.argv) == 2 and sys.argv[1] not in ("-h", "-v", "-s"):
-    try:
-        with open(sys.argv[0], encoding='utf-8') as f:
-            illegal_name_dict = analyse_code(f.read())
-            if len(illegal_name_dict) == 0:
+    if not args or (len(args) == 1 and args[0] == '-h'):
+        sys.stderr.write(
+            '''Usage:
+    checkname filename      : analyse the name in the python code file
+    checkname -s code       : analyse the name in the code string
+    checkname -h            : for help
+    checkname -v            : show the version\n''')
+        return
+
+    if len(args) == 1 and args[0] == '-v':
+        print("version:", __version__)
+        return
+
+    if len(args) == 1 and args[0] not in ("-h", "-v", "-s"):
+        filename = args[0]
+        try:
+            with open(filename, encoding='utf-8') as f:
+                illegal_name_dict = analyse_code(f.read())
+            if not illegal_name_dict:
                 print("no illegal name")
             else:
                 print("found the illegal name below:", end="\n\n", file=sys.stderr)
-                for key, value in illegal_name_dict:
+                for key, value in illegal_name_dict.items():
                     print("name", key, "at",
-                          os.path.abspath(sys.argv[1]), "line",
+                          os.path.abspath(filename), "line",
                           value[0], "offset", value[1], file=sys.stderr)
                 print("please don't try to define a female", file=sys.stderr)
-    except Exception as e:
-        sys.stderr.write(
-            f"""cannot analyse file {sys.argv[1]}: {str(e)}
+        except Exception as e:
+            sys.stderr.write(
+                f"""cannot analyse file {filename}: {str(e)}
 please ensure:
-    1 the file is exist and encode with "utf-8"
-    2 the file is a python code""")
+    1 the file exists and is encoded with "utf-8"
+    2 the file is a python code\n""")
+        return
 
-elif len(sys.argv) == 3 and sys.argv[1] == "-s":
-    try:
-        illegal_name_dict = analyse_code(sys.argv[2])
-        if len(illegal_name_dict) == 0:
-            print("no illegal name")
-        else:
-            print("found the illegal name below:", end="\n\n", file=sys.stderr)
-            for key, value in illegal_name_dict.items():
-                print("name", key, "at",
-                "input", "line",
-                value[0], "offset", value[1], file=sys.stderr)
-            print("please don't try to define a female", file=sys.stderr)
-    except Exception as e:
-        sys.stderr.write(
-            f"""code wrong: {str(e)}""")
-else:
+    if len(args) >= 2 and args[0] == "-s":
+        try:
+            illegal_name_dict = analyse_code(" ".join(args[1:]))
+            if not illegal_name_dict:
+                print("no illegal name")
+            else:
+                print("found the illegal name below:", end="\n\n", file=sys.stderr)
+                for key, value in illegal_name_dict.items():
+                    print("name", key, "at",
+                          "input", "line",
+                          value[0], "offset", value[1], file=sys.stderr)
+                print("please don't try to define a female", file=sys.stderr)
+        except Exception as e:
+            sys.stderr.write(f"""code wrong: {str(e)}\n""")
+        return
+
     sys.stderr.write(
-        f"""Unknown for command {" ".join(sys.argv)}
-Use checkname -h for help.""")
+        f"""Unknown command: {" ".join(sys.argv)}
+Use checkname -h for help.\n""")
+
+
+if __name__ == "__main__":
+    main()
